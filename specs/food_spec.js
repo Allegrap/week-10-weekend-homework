de036600a0bf92c8a94cb8d189bf776f1a6c7e43
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-05-14 17:20:21 +0100

    food spec set up

---
+++ specs/food_spec.js
@@ -0,0 +1,11 @@
+var Food = require('../food.js');
+var assert = require('assert');
+
+describe('food', function(){
+
+  beforeEach(function(){
+    this.food1 = new Food("banana", 8);
+  });
+
+
+})
