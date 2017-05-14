1262af39f4a9fa3f076a2656b48198f936f92bb9
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-05-14 16:48:47 +0100

    task test set up

---
+++ specs/task_spec.js
@@ -0,0 +1,11 @@
+var task = require('../task.js');
+var assert = require('assert');
+
+describe('task', function(){
+
+  beforeEach(function(){
+    this.task = new Task(10, "high", "lordship");
+  });
+
+
+})
